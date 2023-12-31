53707da21dc527d3e422cb4174314fe9b3bcad40
拚拚 <robin92062574@gmail.com>
2023-12-31 16:51:11 +0800

    1231

--- index.py
+++ index.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
 from data import Data
 
 class Main(Data):
@@ -104,4 +105,86 @@ class Main(Data):
         plt.plot(data_Quality)
         plt.show()
 
-Main().test()
+# Main().test()
+    
+class Customer(Data):
+    """顧客分析"""
+    def __init__(self) -> None :
+        # 設定中文字型
+        # plt.rcParams['font.family'] = 'Microsoft YaHei'
+        plt.rcParams['font.family'] = 'Arial Unicode'
+        plt.rcParams['axes.unicode_minus'] = False
+
+    def main(self):
+        # 載入資料集
+        df = super().read()
+
+        # 繪製年齡分佈圖
+        plt.figure(figsize=(10, 6))
+        df['AGE_GROUP'].value_counts().sort_index().plot(kind='bar', color='skyblue')
+        plt.title('顧客年齡分佈')
+        plt.xlabel('年齡組別')
+        plt.ylabel('顧客數量')
+        plt.show()
+
+        # 繪製每個年齡組別的總銷售金額
+        plt.figure(figsize=(10, 6))
+        df.groupby('AGE_GROUP')['AMOUNT'].sum().sort_index().plot(kind='bar', color='lightgreen')
+        plt.title('每個年齡組別的總銷售金額')
+        plt.xlabel('年齡組別')
+        plt.ylabel('總銷售金額')
+        plt.show()
+
+        # 繪製每個PIN碼的顧客數量
+        plt.figure(figsize=(12, 8))
+        df['PIN_CODE'].value_counts().head(20).plot(kind='bar', color='orange')
+        plt.title('每個地區的顧客數量')
+        plt.xlabel('郵遞區號')
+        plt.ylabel('顧客數量')
+        plt.show()
+        
+        # 將 TRANSACTION_DT 轉換為日期類型
+        df['TRANSACTION_DT'] = pd.to_datetime(df['TRANSACTION_DT'])
+        # 計算每位顧客的購買次數
+        customer_purchase_count = df.groupby('CUSTOMER_ID')['TRANSACTION_DT'].count()
+
+        # 初次客與回流客分布
+        plt.figure(figsize=(12, 6))
+
+        # 初次客與回流客分布的長條圖
+        plt.subplot(1, 2, 1)
+        returning_customers = (customer_purchase_count > 1).sum() / len(customer_purchase_count)
+        new_customers = 1 - returning_customers
+        sns.barplot(x=['初次客', '回流客'], y=[new_customers, returning_customers])
+        plt.title('初次客與回流客比例 (長條圖)')
+
+        # 初次客與回流客分布的圓餅圖
+        plt.subplot(1, 2, 2)
+        labels = ['初次客', '回流客']
+        sizes = [new_customers, returning_customers]
+        colors = ['#ff9999','#66b3ff']
+        plt.pie(sizes, labels=labels, autopct='%1.1f%%', colors=colors, startangle=90)
+        plt.title('初次客與回流客比例 (圓餅圖)')
+        plt.tight_layout()
+        plt.show()
+
+        # 計算每位顧客的購買次數和總交易金額
+        customer_summary = df.groupby('CUSTOMER_ID').agg({'TRANSACTION_DT': 'count', 'AMOUNT': 'sum'})
+
+        # 初次客和回流客的交易金額
+        new_customers = customer_summary[customer_summary['TRANSACTION_DT'] == 1]
+        returning_customers = customer_summary[customer_summary['TRANSACTION_DT'] > 1]
+
+        # 計算平均交易金額
+        avg_amount_new_customers = new_customers['AMOUNT'].mean()
+        avg_amount_returning_customers = returning_customers['AMOUNT'].mean()
+
+        plt.figure(figsize=(10, 6))
+        sns.barplot(x=['初次客', '回流客'], y=[avg_amount_new_customers, avg_amount_returning_customers])
+        plt.title('初次客與回流客的平均交易金額')
+        plt.xlabel('顧客類別')
+        plt.ylabel('平均交易金額 (元)')
+        plt.show()
+
+
+Customer().main()
